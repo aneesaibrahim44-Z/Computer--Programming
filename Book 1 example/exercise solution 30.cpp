f480f495117f2d1dadbd27157368ea5558c952eb
Aneesa Ibrahim <aneesaibrahim44@gmail.com>
2026-03-10 11:11:28 +0500

    Add files via upload

---
+++ Book 1 example/exercise solution 30.cpp
@@ -0,0 +1,12 @@
+#include <iostream>   // Library for input and output
+using namespace std;  // Allows us to use cout without std::
+
+int main() {
+
+  string str = "C++ Programming";   // String variable storing text
+
+  // str.size() returns the total number of characters in the string
+  cout << "String Length = " << str.size();
+
+  return 0;   // End of program
+}
