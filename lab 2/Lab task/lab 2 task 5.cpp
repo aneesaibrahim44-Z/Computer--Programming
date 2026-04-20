1e85b0aa7dc7593bfe4c23702c6b52d2f3ad729c
Aneesa Ibrahim <aneesaibrahim44@gmail.com>
2026-04-20 20:04:49 +0500

    Add files via upload

---
+++ lab 2/Lab task/lab 2 task 5.cpp
@@ -0,0 +1,25 @@
+
+// Name: Aneesa ibrahim
+// Department: Electrical Engineering (Power)
+// Registration No: BF25NWELE0739
+// Section: A
+
+#include <iostream>   // library for input and output
+using namespace std;  // allows using cout directly
+
+int main()
+{
+    // printing name 10 times
+    cout << "Aneesa Ibrahim" << endl;
+    cout << "Aneesa Ibrahim" << endl;
+    cout << "Aneesa Ibrahim" << endl;
+    cout << "Aneesa Ibrahim" << endl;
+    cout << "Aneesa Ibrahim" << endl;
+    cout << "Aneesa Ibrahim" << endl;
+    cout << "Aneesa Ibrahim" << endl;
+    cout << "Aneesa Ibrahim" << endl;
+    cout << "Aneesa Ibrahim" << endl;
+    cout << "Aneesa Ibrahim" << endl;
+
+    return 0;  // program ends
+}
