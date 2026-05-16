eccf99e72a3641011a7f2fcf21ce8ee8ebaa44a2
Aneesa Ibrahim <aneesaibrahim44@gmail.com>
2026-05-16 13:50:24 +0500

    Add files via upload

---
+++ lab11/lab 11 tasks/lab 11 task 1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+    // String declaration and initialization
+    string message = "Welcome to C++ Programming";
+
+    // Display string
+    cout << "Message: " << message << endl;
+
+    // Calculate and display length
+    cout << "Length of string = " << message.length() << endl;
+
+    return 0;
+}
---
+++ lab11/lab 11 tasks/lab 11 task 2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+    // String variables
+    string firstName = "Aneesa";
+    string lastName = "Ibrahim";
+
+    // Concatenation
+    string fullName = firstName + " " + lastName;
+
+    // Display full name
+    cout << "Full Name: " << fullName << endl;
+
+    return 0;
+}
